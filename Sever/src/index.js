01ee97d9d49956244b36ecfe3773f22bf6e1c9a2
Vishal Sharma <360vishalsharma.mech@gmail.com>
2023-08-23 11:17:12 +0530

    add

--- Sever/src/index.js
+++ Sever/src/index.js
@@ -4,10 +4,8 @@ const route = require("./Routes/routes")
 require('dotenv').config()
 const cors = require('cors')
 
-
 const app = express()
 app.use(express.json())
-
 
 app.use(cors({origin:"*"}))
 
